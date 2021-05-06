61304af2cc5f3ecf0e2ce6c18c506dfff3a6cff7
FreedSoul <mattwork07@gmail.com>
2021-05-05 22:31:45 -0500

    head snake

--- snake-script.js
+++ snake-script.js
@@ -3,15 +3,71 @@ const ctx = gameFrame.getContext('2d')
 
 let speedOfGame = 7;
 
-//game loop
+//grid system
+let tileCount = 20;
+let tileSize = gameFrame.width / tileCount - 2;
+
+//start snake variables
+let headX = 10;
+let headY = 10;
+
+//movement snake
+let xVelocity = 0;
+let yVelocity = 0;
+
+//game loop - main function
 function drawGame(){
-    clearLoop();
-    
+    //console.log(gameFrame.width)
+    clearScreen()
+    changeSnakePosition()
+    drawSnake()
+    setTimeout(drawGame, 1000/speedOfGame)
+
 }
 
-function clearLoop(){
-
+//update screen
+function clearScreen(){
+    ctx.fillStyle = 'black';
+    ctx.fillRect(0,0,gameFrame.width,gameFrame.height)
 }
+
+//initial position snake
+function drawSnake(){
+    ctx.fillStyle = 'orange';
+    ctx.fillRect(headX*tileCount,headY*tileCount, tileSize,tileSize)
+}
+
+function changeSnakePosition(){
+    headX = headX + xVelocity;
+    headY = headY + yVelocity;
+}
+
+//player actions
+document.body.addEventListener('keydown',KeyDown);
+
+function KeyDown(event){
+    //up
+    if(event.keyCode == 38){
+        xVelocity = 0;
+        yVelocity =-1;
+    }
+    //down
+    if(event.keyCode == 40){
+        xVelocity = 0;
+        yVelocity = 1;
+    }
+    //left
+    if(event.keyCode == 37){
+        xVelocity = -1;
+        yVelocity = 0;
+    }
+    //right
+    if(event.keyCode == 39){
+        xVelocity = 1;
+        yVelocity = 0;
+    }
+}
+
 
 drawGame();
 
