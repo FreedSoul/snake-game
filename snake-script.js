1fda496d88b7f08720ff2260efd1b77d90547470
FreedSoul <mattwork07@gmail.com>
2021-05-05 20:02:05 -0500

    snake initial files

---
+++ snake-script.js
@@ -0,0 +1,17 @@
+const gameFrame = document.getElementById('gameFrame');
+const ctx = gameFrame.getContext('2d')
+
+let speedOfGame = 7;
+
+//game loop
+function drawGame(){
+    clearLoop();
+    
+}
+
+function clearLoop(){
+
+}
+
+drawGame();
+
